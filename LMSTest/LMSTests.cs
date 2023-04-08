8279ca66a7ec22be2612789c4707afc85539a0e3
Robert <ibsonnumber1@gmail.com>
2023-04-07 22:04:41 -0400

    Update project for second submittal

--- LMSTest/LMSTests.cs
+++ LMSTest/LMSTests.cs
@@ -11,24 +11,6 @@ namespace LMSTest
         readonly LMSController controller = new LMSController(Mock.Of<ILogger<LMSController>>());
 
         [TestMethod]
-        public void TestCreateModule()
-        {
-            string expected = "Created Module with id: 10";
-            Module module = new Module { ID = 10, Name = "Week 10" };
-            string actual = controller.CreateModule(module);
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void TestCreateAssignment()
-        {
-            string expected = "Created Assignment with id: 10";
-            Assignment assignment = new Assignment { ID = 10, Name = "Web API", DueDate = DateTime.Now, Grade = 90 };
-            string actual = controller.CreateAssignment(assignment);
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
         public void TestCreateCourse()
         {
             string expected = "Created Course with id: 10";
@@ -38,9 +20,39 @@ namespace LMSTest
         }
 
         [TestMethod]
+        public void TestCreateModule()
+        {
+            string expected = "Created Module with id: 10";
+            Course course = new Course { ID = 10, Name = "Web Architecture" };
+            Module module = new Module { ID = 10, Name = "Week 10", Course = course };
+            controller.CreateCourse(course);
+            string actual = controller.CreateModule(module);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestCreateAssignment()
+        {
+            string expected = "Created Assignment with id: 10";
+            Course course = new Course { ID = 10, Name = "Web Architecture" };
+            Module module = new Module { ID = 10, Name = "Week 10", Course = course };
+            Assignment assignment = new Assignment { ID = 10, Name = "Web API", DueDate = DateTime.Now, Grade = 90, Module = module };
+            controller.CreateCourse(course);
+            controller.CreateModule(module);
+            string actual = controller.CreateAssignment(assignment);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void TestGetAssignment()
         {
-            string expected = "Get Assignment with id: 10";
+            string expected = "ID: 10, Name: LMS, Grade: 10, DueDate: " + DateTime.Now + ", ModuleID: 10";
+            Course course = new Course { ID = 10, Name = "Web Architecture" };
+            Module module = new Module { ID = 10, Name = "Week 10", Course = course };
+            Assignment assignment = new Assignment {ID = 10, Name = "LMS", Grade = 10, DueDate = DateTime.Now, Module = module };
+            controller.CreateCourse(course);
+            controller.CreateModule(module);
+            controller.CreateAssignment(assignment);
             string actual = controller.GetAssignment(10);
             Assert.AreEqual(expected, actual);
         }
@@ -48,7 +60,9 @@ namespace LMSTest
         [TestMethod]
         public void TestGetCourse()
         {
-            string expected = "Get Course with id: 10";
+            string expected = "ID: 10, Name: Web Architecture";
+            Course course = new Course { ID = 10, Name = "Web Architecture" };
+            controller.CreateCourse(course);
             string actual = controller.GetCourse(10);
             Assert.AreEqual(expected, actual);
         }
@@ -56,7 +70,11 @@ namespace LMSTest
         [TestMethod]
         public void TestGetModule()
         {
-            string expected = "Get Module with id: 10";
+            string expected = "ID: 10, Name: Week 10, CourseID: 10";
+            Course course = new Course { ID = 10, Name = "Web Architecture" };
+            Module module = new Module { ID = 10, Name = "Week 10", Course = course };
+            controller.CreateCourse(course);
+            controller.CreateModule(module);
             string actual = controller.GetModule(10);
             Assert.AreEqual(expected, actual);
         }
@@ -109,6 +127,113 @@ namespace LMSTest
         {
             string expected = "Deleted Course with id: 10";
             string actual = controller.DeleteCourse(10);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RequiredTest1()
+        {
+            string expected = "ID: 10, Name: Web Architecture";
+            Course course = new Course { ID = 10, Name = "Web Architecture" };
+            controller.CreateCourse(course);
+            string actual = controller.GetCourse(10);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RequiredTest2()
+        {
+            string expected = "ID: 10, Name: Web ArchitectureID: 10, Name: Week 10, CourseID: 10ID: 20, Name: Week 10, CourseID: 10";
+            Course course = new Course { ID = 10, Name = "Web Architecture" };
+            Module module = new Module { ID = 10, Name = "Week 10", Course = course };
+            Module module2 = new Module { ID = 20, Name = "Week 10", Course = course };
+            controller.CreateCourse(course);
+            controller.CreateModule(module);
+            controller.CreateModule(module2);
+            string actual = controller.GetCourse(10);
+            actual += controller.GetModuleByCourseID(10);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RequiredTest3()
+        {
+            string expected = "ID: 10, Name: Web ArchitectureID: 20, Name: Web ArchitectureID: 30, Name: Web Architecture";
+            Course course = new Course { ID = 10, Name = "Web Architecture" };
+            Course course2 = new Course { ID = 20, Name = "Web Architecture" };
+            Course course3 = new Course { ID = 30, Name = "Web Architecture" };
+            controller.CreateCourse(course);
+            controller.CreateCourse(course2);
+            controller.CreateCourse(course3);
+            string actual = controller.GetCourse(10);
+            actual += controller.GetCourse(20);
+            actual += controller.GetCourse(30);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RequiredTest4()
+        {
+            string expected = "ID: 10, Name: Web API, Grade: 90, DueDate: " + DateTime.Now + ", ModuleID: ID: 30, Name: Web API, Grade: 90, DueDate: " + DateTime.Now + ", ModuleID: ";
+            Assignment assignment = new Assignment { ID = 10, Name = "Web API", DueDate = DateTime.Now, Grade = 90, Module = null };
+            Assignment assignment2 = new Assignment { ID = 20, Name = "Web API", DueDate = DateTime.Now, Grade = 90, Module = null };
+            Assignment assignment3 = new Assignment { ID = 30, Name = "Web API", DueDate = DateTime.Now, Grade = 90, Module = null };
+            controller.CreateAssignment(assignment);
+            controller.CreateAssignment(assignment2);
+            controller.CreateAssignment(assignment3);
+            controller.DeleteAssignment(20);
+            string actual = controller.GetAssignment(10);
+            actual += controller.GetAssignment(20);
+            actual += controller.GetAssignment(30);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReadFullRelationships()
+        {
+            string expected = "ID: 10, Name: Web ArchitectureID: 10, Name: Week 10, CourseID: 10ID: 10, Name: LMS, Grade: 10, DueDate: " + DateTime.Now + ", ModuleID: 10";
+            Course course = new Course { ID = 10, Name = "Web Architecture" };
+            Module module = new Module { ID = 10, Name = "Week 10", Course = course };
+            Assignment assignment = new Assignment { ID = 10, Name = "LMS", Grade = 10, DueDate = DateTime.Now, Module = module };
+            controller.CreateCourse(course);
+            controller.CreateModule(module);
+            controller.CreateAssignment(assignment);
+            string actual = controller.GetCourse(10);
+            actual += controller.GetModule(10);
+            actual += controller.GetAssignment(10);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAssignmentIndependenceAndDependence()
+        {
+            string expected = "ID: 10, Name: LMS, Grade: 10, DueDate: " + DateTime.Now + ", ModuleID: 10ID: 20, Name: LMS, Grade: 10, DueDate: " + DateTime.Now + ", ModuleID: ";
+            Course course = new Course { ID = 10, Name = "Web Architecture" };
+            Module module = new Module { ID = 10, Name = "Week 10", Course = course };
+            Assignment assignment = new Assignment { ID = 10, Name = "LMS", Grade = 10, DueDate = DateTime.Now, Module = module };
+            Assignment assignment2 = new Assignment { ID = 20, Name = "LMS", Grade = 10, DueDate = DateTime.Now, Module = null };
+            controller.CreateCourse(course);
+            controller.CreateModule(module);
+            controller.CreateAssignment(assignment);
+            controller.CreateAssignment(assignment2);
+            string actual = controller.GetAssignment(10);
+            actual += controller.GetAssignment(20);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMultipleAssignmentsUnderModule()
+        {
+            string expected = "ID: 10, Name: LMS, Grade: 10, DueDate: " + DateTime.Now + ", ModuleID: 10ID: 20, Name: LMS, Grade: 10, DueDate: " + DateTime.Now + ", ModuleID: 10";
+            Course course = new Course { ID = 10, Name = "Web Architecture" };
+            Module module = new Module { ID = 10, Name = "Week 10", Course = course };
+            Assignment assignment = new Assignment { ID = 10, Name = "LMS", Grade = 10, DueDate = DateTime.Now, Module = module };
+            Assignment assignment2 = new Assignment { ID = 20, Name = "LMS", Grade = 10, DueDate = DateTime.Now, Module = module };
+            controller.CreateCourse(course);
+            controller.CreateModule(module);
+            controller.CreateAssignment(assignment);
+            controller.CreateAssignment(assignment2);
+            string actual = controller.GetAssignmentByModuleID(10);
             Assert.AreEqual(expected, actual);
         }
     }
--- SimpleLMS/Controllers/LMSController.cs
+++ SimpleLMS/Controllers/LMSController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using System.Data.SQLite;
 
 namespace SimpleLMS.Controllers
 {
@@ -8,28 +11,85 @@ namespace SimpleLMS.Controllers
     {
 
         private readonly ILogger<LMSController> _logger;
+        private SqliteConnection sqliteConnection = new SqliteConnection("Data Source=SimpleLMSDB.db");
 
         public LMSController(ILogger<LMSController> logger)
         {
             _logger = logger;
+            TableSetup();
+        }
+
+        private void TableSetup()
+        {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "DROP TABLE IF EXISTS Assignment";
+            sqliteQuery.ExecuteNonQuery();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "DROP TABLE IF EXISTS Module";
+            sqliteQuery.ExecuteNonQuery();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "DROP TABLE IF EXISTS Course";
+            sqliteQuery.ExecuteNonQuery();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "CREATE TABLE \"Assignment\" ( \"ID\" INTEGER NOT NULL UNIQUE, \"Name\" TEXT NOT NULL, " +
+                "\"Grade\" INTEGER, \"DueDate\" TEXT NOT NULL, \"ModuleID\" INTEGER, PRIMARY KEY(\"ID\" AUTOINCREMENT), " +
+                "FOREIGN KEY(\"ModuleID\") REFERENCES Module(ID) )";
+            sqliteQuery.ExecuteNonQuery();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "CREATE TABLE \"Course\" ( \"ID\" INTEGER NOT NULL UNIQUE, \"Name\" TEXT NOT NULL, PRIMARY KEY(\"ID\") )";
+            sqliteQuery.ExecuteNonQuery();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "CREATE TABLE \"Module\" ( \"ID\" INTEGER NOT NULL UNIQUE, \"Name\" TEXT NOT NULL, \"CourseID\" INTEGER NOT NULL, " +
+                "FOREIGN KEY(\"CourseID\") REFERENCES Course(ID), PRIMARY KEY(\"ID\") )";
+            sqliteQuery.ExecuteNonQuery();
+            sqliteConnection.Close();
         }
 
         //C
         [HttpPost("~/CreateModule")]
         public string CreateModule(Module module)
         {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "INSERT INTO Module(ID, Name, CourseID) VALUES('" + module.ID + "', '" + module.Name + "', '" + module.Course.ID + "')";
+            sqliteQuery.ExecuteNonQuery();
+            sqliteConnection.Close();
             return "Created Module with id: " + module.ID;
         }
 
         [HttpPost("~/CreateAssignment")]
         public string CreateAssignment(Assignment assignment)
         {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            if(assignment.Module == null)
+            {
+                sqliteQuery.CommandText = "INSERT INTO Assignment(ID, Name, Grade, DueDate) VALUES('" + assignment.ID + "', '" + assignment.Name + "', '"
+                    + assignment.Grade + "', '" + assignment.DueDate.ToString() +  "')";
+            }
+            else
+            {
+                sqliteQuery.CommandText = "INSERT INTO Assignment(ID, Name, Grade, DueDate, ModuleID) VALUES('" + assignment.ID + "', '" + assignment.Name + 
+                    "', '" + assignment.Grade + "', '" + assignment.DueDate.ToString() + "', '" + assignment.Module.ID + "')";
+            }
+            sqliteQuery.ExecuteNonQuery();
+            sqliteConnection.Close();
             return "Created Assignment with id: " + assignment.ID;
         }
 
         [HttpPost("~/CreateCourse")]
         public string CreateCourse(Course course)
         {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "INSERT INTO Course(ID, Name) VALUES('" + course.ID + "', '" + course.Name + "')";
+            sqliteQuery.ExecuteNonQuery();
+            sqliteConnection.Close();
             return "Created Course with id: " + course.ID;
         }
 
@@ -37,37 +97,136 @@ namespace SimpleLMS.Controllers
         [HttpGet("~/GetAssignment")]
         public string GetAssignment(int id)
         {
-            return "Get Assignment with id: " + id;
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "SELECT * FROM Assignment WHERE ID = " + id ;
+            SqliteDataReader sQLiteDataReader = sqliteQuery.ExecuteReader();
+            string result = "";
+            while (sQLiteDataReader.Read())
+            {
+                result += "ID: " + sQLiteDataReader["ID"] + ", ";
+                result += "Name: " + sQLiteDataReader["Name"] + ", ";
+                result += "Grade: " + sQLiteDataReader["Grade"] + ", ";
+                result += "DueDate: " + sQLiteDataReader["DueDate"] + ", ";
+                result += "ModuleID: " + sQLiteDataReader["ModuleID"];
+            }
+            sqliteConnection.Close();
+            return result;
         }
 
         [HttpGet("~/GetCourse")]
         public string GetCourse(int id)
         {
-            return "Get Course with id: " + id;
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "SELECT * FROM Course WHERE ID = " + id;
+            SqliteDataReader sQLiteDataReader = sqliteQuery.ExecuteReader();
+            string result = "";
+            while (sQLiteDataReader.Read())
+            {
+                result += "ID: " + sQLiteDataReader["ID"] + ", ";
+                result += "Name: " + sQLiteDataReader["Name"];
+            }
+            sqliteConnection.Close();
+            return result;
         }
 
         [HttpGet("~/GetModule")]
         public string GetModule(int id)
         {
-            return "Get Module with id: " + id;
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "SELECT * FROM Module WHERE ID = " + id;
+            SqliteDataReader sQLiteDataReader = sqliteQuery.ExecuteReader();
+            string result = "";
+            while (sQLiteDataReader.Read())
+            {
+                result += "ID: " + sQLiteDataReader["ID"] + ", ";
+                result += "Name: " + sQLiteDataReader["Name"] + ", ";
+                result += "CourseID: " + sQLiteDataReader["CourseID"];
+            }
+            sqliteConnection.Close();
+            return result;
+        }
+
+        [HttpGet("~/GetModuleByCourseID")]
+        public string GetModuleByCourseID(int id)
+        {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "SELECT * FROM Module WHERE CourseID = " + id;
+            SqliteDataReader sQLiteDataReader = sqliteQuery.ExecuteReader();
+            string result = "";
+            while (sQLiteDataReader.Read())
+            {
+                result += "ID: " + sQLiteDataReader["ID"] + ", ";
+                result += "Name: " + sQLiteDataReader["Name"] + ", ";
+                result += "CourseID: " + sQLiteDataReader["CourseID"];
+            }
+            sqliteConnection.Close();
+            return result;
+        }
+
+        [HttpGet("~/GetAssignmentByModuleID")]
+        public string GetAssignmentByModuleID(int id)
+        {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "SELECT * FROM Assignment WHERE ModuleID = " + id;
+            SqliteDataReader sQLiteDataReader = sqliteQuery.ExecuteReader();
+            string result = "";
+            while (sQLiteDataReader.Read())
+            {
+                result += "ID: " + sQLiteDataReader["ID"] + ", ";
+                result += "Name: " + sQLiteDataReader["Name"] + ", ";
+                result += "Grade: " + sQLiteDataReader["Grade"] + ", ";
+                result += "DueDate: " + sQLiteDataReader["DueDate"] + ", ";
+                result += "ModuleID: " + sQLiteDataReader["ModuleID"];
+            }
+            sqliteConnection.Close();
+            return result;
         }
 
         //U
         [HttpPut("~/UpdateModule")]
         public string UpdateModule(Module module)
         {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "UPDATE Module SET Name = '" + module.Name + "' WHERE ID = " + module.ID;
+            sqliteQuery.ExecuteNonQuery();
+            sqliteConnection.Close();
             return "Updated Module with id: " + module.ID;
         }
 
         [HttpPut("~/UpdateAssignment")]
         public string UpdateAssignment(Assignment assignment)
         {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "UPDATE Assignment SET Name = '" + assignment.Name + "', Grade = " + assignment.Grade + 
+                ", DueDate = '" + assignment.DueDate.ToString() + "' WHERE ID = " + assignment.ID;
+            sqliteQuery.ExecuteNonQuery();
+            sqliteConnection.Close();
             return "Updated Assignment with id: " + assignment.ID;
         }
 
         [HttpPut("~/UpdateCourse")]
         public string UpdateCourse(Course course)
         {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "UPDATE Course SET Name = '" + course.Name + "' WHERE ID = " + course.ID;
+            sqliteQuery.ExecuteNonQuery();
+            sqliteConnection.Close();
             return "Updated Course with id: " + course.ID;
         }
 
@@ -75,18 +234,36 @@ namespace SimpleLMS.Controllers
         [HttpDelete("~/DeleteModule")]
         public string DeleteModule(int id)
         {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "DELETE FROM Module WHERE ID = " + id;
+            sqliteQuery.ExecuteNonQuery();
+            sqliteConnection.Close();
             return "Deleted Module with id: " + id;
         }   
 
         [HttpDelete("~/DeleteAssignment")]
         public string DeleteAssignment(int id)
         {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "DELETE FROM Assignment WHERE ID = " + id;
+            sqliteQuery.ExecuteNonQuery();
+            sqliteConnection.Close();
             return "Deleted Assignment with id: " + id;
         }
 
         [HttpDelete("~/DeleteCourse")]
         public string DeleteCourse(int id)
         {
+            sqliteConnection.Open();
+            var sqliteQuery = new SqliteCommand();
+            sqliteQuery = sqliteConnection.CreateCommand();
+            sqliteQuery.CommandText = "DELETE FROM Course WHERE ID = " + id;
+            sqliteQuery.ExecuteNonQuery();
+            sqliteConnection.Close();
             return "Deleted Course with id: " + id;
         }
     }
